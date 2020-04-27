097639077449b05b6324a589097bcd3167163aed
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-04-27 19:53:41 +0700

    penambahan variabel properties untuk menyimpan hasil json

--- app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
@@ -9,9 +9,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import org.d3if0113.pokepedia.network.PokemonAPI
+import org.d3if0113.pokepedia.property.PokemonRegionProperty
 
 class RegionViewModel(application: Application) : AndroidViewModel(application) {
     private val _response = MutableLiveData<String>()
+    private val _properties = MutableLiveData<List<PokemonRegionProperty>>()
     private var viewModelJob = Job()
     private val coroutineScope = CoroutineScope(
         viewModelJob + Dispatchers.Main
@@ -28,6 +30,7 @@ class RegionViewModel(application: Application) : AndroidViewModel(application) 
             try {
                 var listResult = getPropertiesDeferred.await()
                 _response.value = "Sukses: ${listResult.size} region"
+                _properties.value = listResult
             } catch (e: Exception) {
                 _response.value = "Gagal respon API Region: " + e.message
             }
@@ -36,6 +39,7 @@ class RegionViewModel(application: Application) : AndroidViewModel(application) 
 
     // ----------------------------- public variable & function
     val response: LiveData<String> get() = _response
+    val properties: LiveData<List<PokemonRegionProperty>> get() = _properties
 
     override fun onCleared() {
         super.onCleared()
