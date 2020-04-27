4c3894a0fce7157777507cf9d05127a44126ea32
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-04-27 11:24:51 +0700

    ----- final bottom navigation -----

--- app/src/main/java/org/d3if0113/pokepedia/MainActivity.kt
+++ app/src/main/java/org/d3if0113/pokepedia/MainActivity.kt
@@ -2,11 +2,27 @@ package org.d3if0113.pokepedia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.d3if0113.pokepedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // bottom navigation
+        val bottomNav: BottomNavigationView = binding.btnavNavigation
+        val navController = findNavController(R.id.fragment)
+        val appBarConfig =
+            AppBarConfiguration(setOf(R.id.regionNav, R.id.pokedexNav, R.id.favoriteNav))
+        setupActionBarWithNavController(navController, appBarConfig)
+        bottomNav.setupWithNavController(navController)
     }
 }
