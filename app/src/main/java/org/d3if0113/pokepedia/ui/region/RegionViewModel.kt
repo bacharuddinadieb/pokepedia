d2f97f66a8e23f1b2ccd2c62c50e9bb13c110453
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-04-27 17:30:37 +0700

    memperbarui RegionViewModel menyesuaikan dengan region property

--- app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import org.d3if0113.pokepedia.network.PokemonAPI
+import org.d3if0113.pokepedia.property.PokemonRegionProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -19,13 +20,16 @@ class RegionViewModel(application: Application) : AndroidViewModel(application) 
     private fun getRegionProperties() {
         _response.value = "Respon API Region"
         PokemonAPI.retrofitService.getData().enqueue(
-            object : Callback<String> {
-                override fun onFailure(call: Call<String>, t: Throwable) {
+            object : Callback<List<PokemonRegionProperty>> {
+                override fun onFailure(call: Call<List<PokemonRegionProperty>>, t: Throwable) {
                     _response.value = "Gagal respon API Region: " + t.message
                 }
 
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _response.value = response.body()
+                override fun onResponse(
+                    call: Call<List<PokemonRegionProperty>>,
+                    response: Response<List<PokemonRegionProperty>>
+                ) {
+                    _response.value = "Sukses: ${response.body()?.size} region"
                 }
             }
         )
