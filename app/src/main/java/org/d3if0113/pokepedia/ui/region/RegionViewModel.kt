3ab0d205e8c3e450486eabaadd17e9162a747a08
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-04-27 15:34:52 +0700

    membuat viewmodel region

---
+++ app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
@@ -0,0 +1,29 @@
+package org.d3if0113.pokepedia.ui.region
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import org.d3if0113.pokepedia.network.PokemonAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class RegionViewModel(application: Application) : AndroidViewModel(application) {
+    private val _response = MutableLiveData<String>()
+
+
+    private fun getRegionProperties() {
+        _response.value = "Respon API Region"
+        PokemonAPI.retrofitService.getData().enqueue(
+            object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    _response.value = "Gagal respon API Region: " + t.message
+                }
+
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    _response.value = response.body()
+                }
+            }
+        )
+    }
+}
