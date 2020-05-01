fec9504e57d9ce85ec0a22b46013ed2370505cf7
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 22:24:34 +0700

    pembaruan PokedexFragment dan layoutnya

--- app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/PokedexFragment.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/PokedexFragment.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import org.d3if0113.pokepedia.R
+import org.d3if0113.pokepedia.databinding.FragmentPokedexBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class PokedexFragment : Fragment() {
 
+    private lateinit var binding: FragmentPokedexBinding
+    private val viewModel: PokedexViewModel by lazy {
+        ViewModelProviders.of(this).get(PokedexViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pokedex, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_pokedex, container, false)
+        binding.lifecycleOwner = this
+        binding.pokedexViewModel = viewModel
+
+        return binding.root
     }
 
 }
