5af7261c510958fa621c2998f4a1fb5e145c213c
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 21:50:26 +0700

    penambahan PokedexViewModel

---
+++ app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/PokedexViewModel.kt
@@ -0,0 +1,6 @@
+package org.d3if0113.pokepedia.ui.pokedex
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class PokedexViewModel(application: Application) : AndroidViewModel(application)
