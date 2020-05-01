e9026cae22426f50ce6f79eee1d1eb700c45b721
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 13:53:51 +0700

    pembuatan class entityregion dan entitykota

---
+++ app/src/main/java/org/d3if0113/pokepedia/database/RegionEntities.kt
@@ -0,0 +1,39 @@
+package org.d3if0113.pokepedia.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tb_region")
+data class EntityRegion(
+    @PrimaryKey()
+    @ColumnInfo(name = "nama_region")
+    val nama: String,
+
+    @ColumnInfo(name = "deskripsi_region")
+    val deskripsi: String
+)
+
+@Entity(
+    tableName = "tb_kota",
+    foreignKeys = arrayOf(
+        ForeignKey(
+            entity = EntityRegion::class,
+            parentColumns = arrayOf("nama_region"),
+            childColumns = arrayOf("id_region"),
+            onDelete = ForeignKey.CASCADE
+        )
+    )
+)
+data class EntityKota constructor(
+    @PrimaryKey()
+    @ColumnInfo(name = "nama_kota")
+    val nama: String,
+
+    @ColumnInfo(name = "slogan_kota")
+    val slogan: String,
+
+    @ColumnInfo(name = "id_region")
+    val idRegion: String
+)
