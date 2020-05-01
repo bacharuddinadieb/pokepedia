2126f4d3dfa9ec640296f75920c5f931a2413d4f
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 15:29:12 +0700

    ----- perbaikan RegionViewModel untuk mengambil data dari room -----

--- app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/region/RegionViewModel.kt
@@ -1,19 +1,21 @@
 package org.d3if0113.pokepedia.ui.region
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.d3if0113.pokepedia.database.getDatabase
-import org.d3if0113.pokepedia.network.PokemonAPI
 import org.d3if0113.pokepedia.network.PokemonAPIStatus
 import org.d3if0113.pokepedia.property.PokemonRegionProperty
 import org.d3if0113.pokepedia.repository.RegionRepository
 
 class RegionViewModel(application: Application) : AndroidViewModel(application) {
     private val _status = MutableLiveData<PokemonAPIStatus>()
-    private val _properties = MutableLiveData<List<PokemonRegionProperty>>()
     private val _navigateToDetailRegion = MutableLiveData<PokemonRegionProperty>()
     private val _regionRepository = RegionRepository(getDatabase(application))
     private var viewModelJob = Job()
@@ -26,25 +28,20 @@ class RegionViewModel(application: Application) : AndroidViewModel(application) 
     }
 
     private fun getRegionProperties() {
-        _status.value = PokemonAPIStatus.LOADING
+        _status.value = PokemonAPIStatus.DONE
         coroutineScope.launch {
-            var getPropertiesDeferred = PokemonAPI.retrofitService.getData()
             try {
-                var listResult = getPropertiesDeferred.await()
-                _properties.value = listResult
                 _regionRepository.refreshRegion()
-                _status.value = PokemonAPIStatus.DONE
             } catch (e: Exception) {
-                delay(2500)
-                _status.value = PokemonAPIStatus.ERROR
-                _properties.value = ArrayList()
+                // _status.value = PokemonAPIStatus.ERROR
+                Log.i("Error Ngga tau :'v", e.message)
             }
         }
     }
 
     // ----------------------------- public variable & function
     val status: LiveData<PokemonAPIStatus> get() = _status
-    val properties: LiveData<List<PokemonRegionProperty>> get() = _properties
+    val properties: LiveData<List<PokemonRegionProperty>> = _regionRepository.regionJoin
     val navigateToDetailRegion: LiveData<PokemonRegionProperty> get() = _navigateToDetailRegion
 
     fun navigatedToDetailRegion() {
