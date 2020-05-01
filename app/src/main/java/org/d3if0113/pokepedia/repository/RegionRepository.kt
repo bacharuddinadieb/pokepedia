1e82163e2269e883c5adbc2681d3d26f4ca9941d
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 15:20:59 +0700

    penambahan var regionJoin untuk menampung list PokemonRegionProperty

--- app/src/main/java/org/d3if0113/pokepedia/repository/RegionRepository.kt
+++ app/src/main/java/org/d3if0113/pokepedia/repository/RegionRepository.kt
@@ -1,14 +1,23 @@
 package org.d3if0113.pokepedia.repository
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.d3if0113.pokepedia.database.EntityKota
 import org.d3if0113.pokepedia.database.PokemonDatabase
 import org.d3if0113.pokepedia.database.asDatabaseModelRegion
+import org.d3if0113.pokepedia.database.asDomainModelRegionJoin
 import org.d3if0113.pokepedia.network.PokemonAPI
+import org.d3if0113.pokepedia.property.PokemonRegionProperty
 
 class RegionRepository(private val database: PokemonDatabase) {
+
+    val regionJoin: LiveData<List<PokemonRegionProperty>> =
+        Transformations.map(database.regionDAO.getAllRegionJoinKota()) {
+            it.asDomainModelRegionJoin()
+        }
 
     suspend fun refreshRegion() {
         withContext(Dispatchers.IO) {
