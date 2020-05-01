aca7c36c73ddb32c127849ec6c42e251569402ab
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-01 22:23:44 +0700

    pembaruan PokedexViewModel

--- app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/PokedexViewModel.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/PokedexViewModel.kt
@@ -1,6 +1,48 @@
 package org.d3if0113.pokepedia.ui.pokedex
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.d3if0113.pokepedia.network.PokemonAPI
+import org.d3if0113.pokepedia.property.PokemonPokedexProperty
 
-class PokedexViewModel(application: Application) : AndroidViewModel(application)
+class PokedexViewModel(application: Application) : AndroidViewModel(application) {
+    private val _properties = MutableLiveData<List<PokemonPokedexProperty>>()
+
+    private var viewModelJob = Job()
+    private val coroutineScope = CoroutineScope(
+        viewModelJob + Dispatchers.Main
+    )
+
+    init {
+        getPokedexProperties()
+    }
+
+    private fun getPokedexProperties() {
+        coroutineScope.launch {
+            var getPropertiesDeferred = PokemonAPI.retrofitService.getDataPokedex()
+            try {
+                var listResult = getPropertiesDeferred.await()
+                _properties.value = listResult
+                Log.i("Data Pokemon", "a ${listResult.size}")
+            } catch (e: Exception) {
+                // _status.value = PokemonAPIStatus.ERROR
+                Log.i("Error Ngga tau :'v", e.message)
+            }
+        }
+    }
+
+    // ----------------------------- public variable & function
+    val properties: LiveData<List<PokemonPokedexProperty>> get() = _properties
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
