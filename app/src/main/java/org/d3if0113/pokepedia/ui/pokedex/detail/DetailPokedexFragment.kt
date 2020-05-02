e6168ceb66ba64798c592fb1a3e363ef54ca214f
bacharuddinadieb <bacharuddinadiebpratama@gmail.com>
2020-05-02 14:59:45 +0700

    perbaikan DetailPokedexFragment dengan menampilkan data dari parcel arguments

--- app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/detail/DetailPokedexFragment.kt
+++ app/src/main/java/org/d3if0113/pokepedia/ui/pokedex/detail/DetailPokedexFragment.kt
@@ -1,23 +1,34 @@
 package org.d3if0113.pokepedia.ui.pokedex.detail
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import org.d3if0113.pokepedia.R
+import org.d3if0113.pokepedia.databinding.FragmentDetailPokedexBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class DetailPokedexFragment : Fragment() {
 
+    private lateinit var binding: FragmentDetailPokedexBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_pokedex, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_detail_pokedex, container, false)
+        val args = arguments?.let { DetailPokedexFragmentArgs.fromBundle(it) }
+        val pokemonPokedexProperty = args!!.SELECTEDPOKEDEXPROPERTY
+
+        Log.i("detail pokemon", "${pokemonPokedexProperty.nama}")
+
+        return binding.root
     }
 
 }
